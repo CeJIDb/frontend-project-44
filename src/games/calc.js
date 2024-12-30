ac7ba66ce24e3662b0f91bcf3af7962247e8198d
cejidb <cejidb@mail.ru>
2024-12-30 15:28:35 +0300

    feat: implement logic for brain-calc game

---
+++ src/games/calc.js
@@ -0,0 +1,35 @@
+import { getRandomInt, getRandomOperation } from '../utils.js';
+
+const description = 'What is the result of the expression?';
+
+const operations = ['+', '-', '*'];
+
+const generateRound = () => {
+  const num1 = getRandomInt(1, 100);
+  const num2 = getRandomInt(1, 100);
+  const operation = getRandomOperation(operations);
+  const question = `${num1} ${operation} ${num2}`;
+
+  let correctAnswer;
+
+  switch (operation) {
+    case '+':
+      correctAnswer = num1 + num2;
+      break;
+    case '-':
+      correctAnswer = num1 - num2;
+      break;
+    case '*':
+      correctAnswer = num1 * num2;
+      break;
+    default:
+      correctAnswer = num1 + num2;
+      break;
+  }
+
+  correctAnswer = String(correctAnswer);
+
+  return { question, correctAnswer };
+};
+
+export { description, generateRound };
