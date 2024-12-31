e09521b01f834f84f42519fe0a8e0eb1b845d5ea
cejidb <cejidb@mail.ru>
2024-12-31 11:40:48 +0300

    feat: implement logic for progression game

---
+++ src/games/progression.js
@@ -0,0 +1,24 @@
+import { getRandomInt } from '../utils.js';
+
+const generateProgression = (start, step, length) => {
+  return Array.from({ length }, (_, i) => start + i * step);
+};
+
+const description = 'What number is missing in the progression?';
+
+const generateRound = () => {
+  const start = getRandomInt(1, 100);
+  const length = getRandomInt(5, 10);
+  const step = getRandomInt(-15, 15);
+  const progression = generateProgression(start, step, length);
+  const missedNumIndex = getRandomInt(0, length - 1);
+  const correctAnswer = String(progression[missedNumIndex]);
+
+  progression[missedNumIndex] = '..';
+
+  const question = progression.join(' ');
+
+  return { question, correctAnswer };
+};
+
+export { description, generateRound };
