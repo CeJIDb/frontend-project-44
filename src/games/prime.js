703473613350e5149136fe383470c1e35ddd3d8f
cejidb <cejidb@mail.ru>
2024-12-31 12:05:47 +0300

    feat: implement prime number game logic

---
+++ src/games/prime.js
@@ -0,0 +1,20 @@
+import { getRandomInt } from '../utils.js';
+
+const isPrime = (n) => {
+  if (n <= 1) return false;
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) return false;
+  }
+  return true;
+};
+
+const description =
+  'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const generateRound = () => {
+  const question = getRandomInt(1, 100);
+  const correctAnswer = isPrime(question) ? 'yes' : 'no';
+  return { question, correctAnswer };
+};
+
+export { description, generateRound };
