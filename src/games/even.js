8f2ac3d4d737f8596087636e91175dce94a4ed05
cejidb <cejidb@mail.ru>
2024-12-31 10:53:08 +0300

    refactor: move isEven function from even.js to utils.js for reuse across games

--- src/games/even.js
+++ src/games/even.js
@@ -1,10 +1,8 @@
-import { getRandomInt } from '../utils.js';
+import { getRandomInt, isEven } from '../utils.js';
 
 const description =
   'Answer "yes" if the number is even, otherwise answer "no".';
 const maxNumber = 100;
-
-const isEven = (number) => number % 2 === 0;
 
 const generateRound = () => {
   const question = getRandomInt(1, maxNumber);
--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,7 @@
 export const getRandomInt = (min, max) =>
   Math.floor(Math.random() * (max - min + 1)) + min;
+
 export const getRandomOperation = (operations) =>
   operations[Math.floor(Math.random() * operations.length)];
+
+export const isEven = (number) => number % 2 === 0;
